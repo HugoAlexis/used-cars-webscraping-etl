009b4c7296a846669d2a0639a8c431576c9c498c
hugoalexis <hugoalexistorrespasillas@gmail.com>
2025-11-29 13:59:20 -0600

    ORM Model implemented for Sites table

---
+++ src/orm/ORMModels.py
@@ -0,0 +1,9 @@
+from ..orm.base import BaseORMModel
+
+class Site(BaseORMModel):
+    table_name = 'sites'
+    table_columns = ['site_id', 'name', 'base_url']
+    table_id = ['site_id']
+
+    def __init__(self, name, base_url, **kwargs):
+        super().__init__(name=name, base_url=base_url, **kwargs)
